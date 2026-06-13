34cd3c64dc2964c8a12cd0740bad6bb0532afd0c
Himanshu-singh-cse <himanshusingh20032007@gmail.com>
2026-06-13 17:30:28 +0530

    Day 2: Added STL and Logic problems

---
+++ stl.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    unordered_set<int>st={2,3,4,5,6};
+    for(int i=0;i<st.size();i++){
+        cout<<st[i];
+    }
+
+    return 0;
+
+    
+
+}
