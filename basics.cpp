77d3e24b19f5ac462279487991560d26e48ce8b4
Himanshu-singh-cse <himanshusingh20032007@gmail.com>
2026-06-20 00:04:49 +0530

    Day 3: Added basics and updated stl files

---
+++ basics.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){;
+
+    
+    int age;
+    cin>>age;
+    if(age>=18){
+    cout<<"eligibkle to drive ";
+    }else{
+        cout<<"not eligible ";
+    }
+
+
+
+
+
+    
+    return 0;
+}
--- stl.cpp
+++ stl.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    unordered_set<int>st={2,3,4,5,6};
-    for(int i=0;i<st.size();i++){
-        cout<<st[i];
-    }
-
-    return 0;
-
+    unordered_set<int> st={2,3,4,5};
+    for(auto it = st.begin();it !=st.end();it++)
     
+{
+         cout<<*it<<" ";
+}
+return 0;
 
 }
