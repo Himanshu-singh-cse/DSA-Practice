8dde8878003843b5bae4948fcd17bf7e3f933a5e
Himanshu-singh-cse <himanshusingh20032007@gmail.com>
2026-06-26 08:40:19 +0530

    Add Day 4 of recursion solution

---
+++ recussion2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void printSubsets(vector<int> &arr,vector<int>&ans,int i){
+    if(i ==arr.size()){
+        for(int val:arr){
+            cout<<val<<" ";
+
+        }
+        cout<<endl;
+        return ;
+
+    }
+    ans.push_back(arr[i]);
+    printSubsets(arr,ans,i+1);
+    ans.pop_back();
+    //exclude
+    printSubsets(arr,ans,i+1);
+
+}
+int main(){
+    vector<int> arr ={1,2,3};
+    vector<int>ans;
+}
